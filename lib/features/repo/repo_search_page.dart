ba644486bf1a148b6e61d93d7431c1fe4a6fb2ab
mono <mono0926@gmail.com>
2022-08-08 10:51:23 +0900

    Revert "例えばこのようにすると再読み込み中に、直前のtotalCount値になってローディング表示されない"
    This reverts commit 84dadcf1e8d74fcd10d02798e6043fcb43107a81.

--- lib/features/repo/repo_search_page.dart
+++ lib/features/repo/repo_search_page.dart
@@ -14,7 +14,6 @@ class RepoSearchPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
     const horizontalPadding = 16.0;
-    final totalCount = ref.watch(repoTotalCountProvider);
     return Column(
       children: [
         const SafeArea(
@@ -28,49 +27,52 @@ class RepoSearchPage extends ConsumerWidget {
         ),
         const Divider(),
         Expanded(
-          child: totalCount == null
-              ? const CenteredCircularProgressIndicator()
-              : Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: horizontalPadding,
-                          vertical: 4,
-                        ),
-                        child: Text(
-                          context.l10n.searchResultTotalCount(totalCount),
-                          style: theme.textTheme.bodySmall,
-                        ),
+          child: AsyncValueBuilder<int>(
+            value: ref.watch(repoTotalCountProvider),
+            onRefresh: () => ref.refresh(searchRepoPagingProvider(1).future),
+            builder: (totalCount) {
+              return Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: horizontalPadding,
+                        vertical: 4,
+                      ),
+                      child: Text(
+                        context.l10n.searchResultTotalCount(totalCount),
+                        style: theme.textTheme.bodySmall,
                       ),
                     ),
-                    Expanded(
-                      child: totalCount < 1
-                          ? const RepoNotFound()
-                          : ListView.separated(
-                              itemCount: totalCount,
-                              separatorBuilder: (context, _) => const Divider(),
-                              itemBuilder: (context, index) {
-                                final currentRepoFromIndex = ref
-                                    .watch(
-                                        searchRepoPagingProvider(index ~/ 30))
-                                    .whenData(
-                                      (paging) => paging.items[index % 30],
-                                    );
-                                return ProviderScope(
-                                  overrides: [
-                                    currentRepoProvider.overrideWithValue(
-                                      currentRepoFromIndex,
-                                    )
-                                  ],
-                                  child: const RepoTile(),
-                                );
-                              },
-                            ),
-                    ),
-                  ],
-                ),
+                  ),
+                  Expanded(
+                    child: totalCount < 1
+                        ? const RepoNotFound()
+                        : ListView.separated(
+                            itemCount: totalCount,
+                            separatorBuilder: (context, _) => const Divider(),
+                            itemBuilder: (context, index) {
+                              final currentRepoFromIndex = ref
+                                  .watch(searchRepoPagingProvider(index ~/ 30))
+                                  .whenData(
+                                    (paging) => paging.items[index % 30],
+                                  );
+                              return ProviderScope(
+                                overrides: [
+                                  currentRepoProvider.overrideWithValue(
+                                    currentRepoFromIndex,
+                                  )
+                                ],
+                                child: const RepoTile(),
+                              );
+                            },
+                          ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ],
     );
